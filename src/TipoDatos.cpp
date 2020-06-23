78940a9c6beab77082d4709c08c48b2ed42d86cf
Nestor <nestor2502@ciencias.unam.mx>
2020-06-23 15:07:14 -0500

    tipos de datos

---
+++ src/TipoDatos.cpp
@@ -0,0 +1,23 @@
+#include<iostream> //libreria principal 
+
+using namespace std;
+
+/**
+ * Funcion principal
+ * */
+int main(){
+    //tipos de datos principales
+    int numero  = 15;
+    float flotante  = 15.36;
+    double doble = 15.5548;
+    char caracter = 'a';
+    
+
+    cout<<numero;
+    cout<<flotante;
+    cout<<doble;
+    cout<<caracter;
+
+
+    return 0;
+}
