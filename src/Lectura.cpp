97cd912ccb3060ba2d3b9ff8a29091bf680ada13
nestor2502 <nestor2502@ciencias.unam.mx>
2020-07-27 00:52:03 -0500

    Se usa la entrada estandar en c++

---
+++ src/Lectura.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero; 
+    cout<<"Digita un numero: ";
+    cin>>numero; // aqui se almacena el numero que ingresa el usuario
+    cout<<"El numero que digito es: "<<numero; //se muestra el numero al usuario
+
+    return 0; 
+}
